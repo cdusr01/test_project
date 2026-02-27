17f02842d8f23eaa8bdbb71f857b407b85dd1baa
cdusr01 <newmail1728@gmail.com>
2026-02-27 17:37:39 +0300

    some changes

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.an
 if __name__ == '__main__':
     print_hi('PyCharm')
 
